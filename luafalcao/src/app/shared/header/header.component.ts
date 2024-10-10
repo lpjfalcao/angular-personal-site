fd634d7e9cdaad05380c71906e0b321eb1bd0be5
Luã Falcão <=>
2024-10-10 10:45:22 -0300

    Including responsive design for header

--- luafalcao/src/app/shared/header/header.component.ts
+++ luafalcao/src/app/shared/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
