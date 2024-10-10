7e3c03632ff27d6bc2f1b69fd964d9fdb6992a03
Luã Falcão <=>
2024-10-10 12:54:32 -0300

    Including assets and component to show my experience

--- luafalcao/src/app/pages/home/components/companies/companies.component.ts
+++ luafalcao/src/app/pages/home/components/companies/companies.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { MissionStatementComponent } from '../../../../shared/mission-statement/mission-statement.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-companies',
   standalone: true,
-  imports: [MissionStatementComponent],
+  imports: [MissionStatementComponent, CommonModule],
   templateUrl: './companies.component.html',
   styleUrl: './companies.component.scss'
 })
 export class CompaniesComponent {
-
+  empresas = [
+    { nome: 'First Decision', logo: 'assets/img/fd-logo.png' },
+    { nome: 'ATSNET Soluções em TI', logo: 'assets/img/atsnet-logo.svg' },
+    { nome: 'Infobase IT', logo: 'assets/img/infobase-logo.png' },
+    { nome: 'IBM', logo: 'assets/img/ibm-logo.png' },
+    { nome: 'Perlink', logo: 'assets/img/perlink-logo.png' },
+    { nome: 'Alterdata Software', logo: 'assets/img/alterdata-logo.png' },
+    { nome: 'Agile Content', logo: 'assets/img/agile-content.png' },
+    { nome: 'Wipro', logo: 'assets/img/wipro.png' },
+  ];
 }
