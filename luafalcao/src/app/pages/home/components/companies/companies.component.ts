c412ee3b157e7a40d471546ae20071f899b9ec05
Luã Falcão <=>
2024-10-10 13:08:13 -0300

    Creating new section for companies

--- luafalcao/src/app/pages/home/components/companies/companies.component.ts
+++ luafalcao/src/app/pages/home/components/companies/companies.component.ts
@@ -11,13 +11,18 @@ import { CommonModule } from '@angular/common';
 })
 export class CompaniesComponent {
   empresas = [
-    { nome: 'First Decision', logo: 'assets/img/fd-logo.png' },
-    { nome: 'ATSNET Soluções em TI', logo: 'assets/img/atsnet-logo.svg' },
-    { nome: 'Infobase IT', logo: 'assets/img/infobase-logo.png' },
-    { nome: 'IBM', logo: 'assets/img/ibm-logo.png' },
-    { nome: 'Perlink', logo: 'assets/img/perlink-logo.png' },
-    { nome: 'Alterdata Software', logo: 'assets/img/alterdata-logo.png' },
-    { nome: 'Agile Content', logo: 'assets/img/agile-content.png' },
-    { nome: 'Wipro', logo: 'assets/img/wipro.png' },
+    { nome: 'First Decision', logo: 'assets/img/fd-logo.png', url: 'https://www.firstdecision.com.br/' },
+    { nome: 'ATSNET Soluções em TI', logo: 'assets/img/atsnet-logo.svg', url: 'https://www.atsnet.com.br/' },
+    { nome: 'Infobase IT', logo: 'assets/img/infobase-logo.png', url: 'https://www.infobase.com.br/' },
+    { nome: 'IBM', logo: 'assets/img/ibm-logo.png', url: 'https://www.ibm.com/' },
+    { nome: 'Perlink', logo: 'assets/img/perlink-logo.png', url: 'https://www.perlink.com.br/' },
+    { nome: 'Alterdata Software', logo: 'assets/img/alterdata-logo.png', url: 'https://www.alterdata.com.br/' },
+    { nome: 'Agile Content', logo: 'assets/img/agile-content.png', url: 'https://www.agilecontent.com.br/' },
+    { nome: 'Wipro', logo: 'assets/img/wipro.png', url: 'https://www.wipro.com/' },
   ];
+
+  redirecionarParaEmpresa(url: string) {
+    window.open(url, '_blank');
+  } 
 }
+
--- luafalcao/src/app/pages/home/components/technologies/technologies.component.ts
+++ luafalcao/src/app/pages/home/components/technologies/technologies.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MissionStatementComponent } from '../../../../shared/mission-statement/mission-statement.component';
 
 @Component({
   selector: 'app-technologies',
   standalone: true,
-  imports: [],
+  imports: [MissionStatementComponent],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.scss'
 })
