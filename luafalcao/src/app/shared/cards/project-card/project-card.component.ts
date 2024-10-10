b5c809cb815c09a2d39134aa93d7d0576714bf3d
Luã Falcão <=>
2024-10-10 07:57:51 -0300

    Implementing project section

--- luafalcao/src/app/shared/cards/project-card/project-card.component.ts
+++ luafalcao/src/app/shared/cards/project-card/project-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
@@ -13,4 +14,5 @@ export class ProjectCardComponent {
   @Input() description: string = '';
   @Input() projectType: string = '';
   @Input() projectLink: string = '';
+  @Input() projectTypeStyle: string = '';
 }
