e1958eb710dc0981c486b7d3ff67d618be3b3df9
Luã Falcão <=>
2024-10-12 07:13:21 -0300

    AJustando seção de projetos para incluir novos projetos e um botão ver mais

--- luafalcao/src/app/pages/home/components/projects/projects.component.ts
+++ luafalcao/src/app/pages/home/components/projects/projects.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { ProjectCardComponent } from '../../../../shared/cards/project-card/project-card.component';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, MatIconModule, CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  showAllProjects = false;
 
+  showProjects() {
+    this.showAllProjects = true;
+  }
 }
