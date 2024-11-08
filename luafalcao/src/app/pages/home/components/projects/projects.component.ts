b316a122e87ca0f983d179d4337ac779da90aeb4
lpjfalcao <lpjfalcao@gmail.com>
2024-11-08 14:38:09 -0300

    Ajustes na seção de projetos

--- luafalcao/src/app/pages/home/components/projects/projects.component.ts
+++ luafalcao/src/app/pages/home/components/projects/projects.component.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { HomeService } from '../../home.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
@@ -18,27 +19,59 @@ export class ProjectsComponent {
   public mainProjects: any[] = [];
   public personalProjects: any[] = [];
 
-  constructor(private homeService: HomeService) {
+  constructor(private router: Router) {
     console.log(`Versão 1.0.0 - ${new Date()}`);
   }
 
-  async ngOnInit() {
-    try {
-      this.homeService.getAllProjects().subscribe((data) =>
+  ngOnInit() { 
+    this.projects.push(
       {
-        this.mainProjects = data as any[]; 
-        this.mainProjects.sort((a, b) => a.id - b.id);
-        this.personalProjects = this.mainProjects.slice(-3); 
-        this.mainProjects = this.mainProjects.slice(0, this.mainProjects.length - 3); 
-      }); 
-     
-    } catch (err) {
-      console.log(err);
-    }
+        imagem: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/jucerja.png',
+        nome: 'Portal Web JUCERJA',
+        descricao: 'Enquanto estive alocado na Junta Comercial do Estado do Rio de Janeiro durante o período de 1 ano e 7 meses participei do projeto de migração tecnológica do seu Portal Web Institucional onde trabalhei com front-end criando interfaces de usuário com design responsivo em HTML5, CSS3, jQuery e ASP .NET MVC5.',
+        tipo: 'FRONT-END',
+        link: 'https://www.jucerja.rj.gov.br/'
+      },
+      {
+        imagem: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/braslight.png',
+        nome: 'Portal Web Braslight',
+        descricao: 'Fiz a migração tecnológica do Portal Web da Braslight Seguros durante 1 ano para uma tecnologia mais moderna, onde fiquei responsável pelo desenvolvimento de APIs com ASP .NET Web API e a construção de interfaces de usuário em AngularJS, HTML5, CSS3 e JavaScript. Também trabalhei com o Umbraco para gerenciamento de conteúdo das telas e ADO .NET e SQL Server para manipulação e armazenamento dos dados.',
+        tipo: 'FULL-STACK',
+        link: 'https://www.braslight.com.br/v3'
+      },
+      {
+        imagem: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/econtador.png',
+        nome: 'e-Contador',
+        descricao: 'Na empresa Alterdata fiquei responsável pelo desenvolvimento do app e-Contador, uma solução  para o setor contábil disponível para iPhones e iPads. Fiz a atualização do app na App Store além de implementar funcionalidades como Push Notifications usando o serviço Firebase Cloud Message, download de documentos e telemetria com uso do Google Analytics.',
+        tipo: 'MOBILE',
+        link: 'https://apps.apple.com/br/app/alterdata-e-contador/id1411356193'
+      },
+      {
+        imagem: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/meuproconrj.png',
+        nome: 'Meu PROCON RJ',
+        descricao: 'Colaborei no desenvolvimento do app Meu PROCON RJ onde fiquei responsável pela construção de interfaces de usuário com design responsivo para plataformas Android e iOS, além de ajudar na publicação do aplicativo na App Store. Trabalhei com tecnologias como Phonegap, Cordova, HTML5, CSS3, JavaScript e Boostrap.',
+        tipo: 'MOBILE',
+        link: 'http://www.procon.rj.gov.br/index.php/publicacao/detalhar/1573'
+      },
+      {
+        imagem: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/capemisa.png',
+        nome: 'Portal Web Capemisa',
+        descricao: 'Enquanto estive alocado na Capemisa Seguros fiquei responsável pela manutenção do seu Portal Web incluindo novos componentes na interface e fazendo integração com APIs para exibir informaçãoes relevantes para o usuário no sistema referente a locais de atendimento.',
+        tipo: 'FULL-STACK',
+        link: 'https://www.capemisa.com.br/'
+      },
+      {
+        imagem: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/personal.png',
+        nome: 'Personal Store',
+        descricao: 'Atuei como Full-Stack em um projeto de uma loja de distribuição de conteúdo online para o cliente Argentino Personal onde atuei desenvolvendo funcionalidades em Node.js como autorização de APIs e componentes de inteface de usuário para um gerenciador de conteúdo (CMS). Além disso fiz o provisionamento de infraestrutura na AWS usando a ferramenta Terraform e publiquei aplicações usando Docker nos serviços AWS ECR e AWS ECS.',
+        tipo: 'FULL-STACK',
+        link: 'https://tienda.personal.com.ar/'
+      }    
+    )  
   }
 
 
-  showProjects() {
-    this.showAllProjects = true;
+  goToProjects() {
+    this.router.navigate(['/projetos']);
   }
 }
