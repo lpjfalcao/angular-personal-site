292339e2be5c7d6988ce045ae7a12d4faf0edca9
lpjfalcao <lpjfalcao@gmail.com>
2025-03-15 09:39:50 -0300

    Updating texts to English

--- src/app/pages/home/components/companies/companies.component.ts
+++ src/app/pages/home/components/companies/companies.component.ts
@@ -22,6 +22,7 @@ export class CompaniesComponent {
     { nome: 'Alterdata Software', logo: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/alterdata-logo.png', url: 'https://www.alterdata.com.br/' },
     { nome: 'Agile Content', logo: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/agile-content.png', url: 'https://www.agilecontent.com/' },
     { nome: 'Wipro', logo: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/wipro.png', url: 'https://www.wipro.com/' },
+    { nome: 'AGL Solutions', logo: 'https://luafalcao-site-pessoal.s3.amazonaws.com/images/agl-logo3.png', url: 'https://aglsolutions.com.br/' },
   ];
 
   responsiveOptions = [
--- src/app/shared/mission-statement/mission-statement.component.ts
+++ src/app/shared/mission-statement/mission-statement.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './mission-statement.component.scss'
 })
 export class MissionStatementComponent {
-  missionText = `"Eu uso o meu conhecimento em tecnologia para desenvolver soluções que agregam valor às empresas e aos usuários."`;
+  missionText = `"I use my knowledge in technology to develop solutions that add value to companies and users."`;
 }
